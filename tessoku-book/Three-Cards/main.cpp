1cd5269b6a07a04f265449100e9869eac9573767
kobakboa5884 <kobanminmin58@gmail.com>
2024-08-18 22:24:04 +0900

    feat: second submit

--- tessoku-book/Three-Cards/main.cpp
+++ tessoku-book/Three-Cards/main.cpp
@@ -2,29 +2,25 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int N, K;
 
     cin >> N >> K;
 
     int cnt = 0;
 
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= N ; j++){
-            int red = i;
-            int brue = j;
-            int white = K - (red + brue);
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= N; j++)
+        {
+            int white = K - (i + j);
 
-            if(white > N or white <= 0){
+            if (white > N or white <= 0)
+            {
                 continue;
             }
-            int sum = red + brue + white;
-
-            // cout << red << " " << brue << " " << white << endl; 
-
-            if(sum == K){
-                cnt += 1;
-            }
+            cnt += 1;
         }
     }
 
